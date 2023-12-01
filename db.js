3f6c6e1660e63f8f9be1583f14d096eba7821395
ProMLGHacher <araik.gukasyan00@mail.ru>
2023-12-01 10:48:07 +0500

    comm

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 import postgres from 'postgres'
 
+//подключение к базе данных
 export const sql = postgres({
     host: 'localhost',
     port: 5432,
--- index.js
+++ index.js
@@ -4,10 +4,14 @@ import { register } from "./controllers/register.js";
 import { auth } from "./controllers/auth.js";
 import { roleMiddleware } from "./middlewares/roleMiddleware.js";
 
+
+//порт на котором будет работать сервер
 const PORT = 3000
 
+//сама переменная сервера
 const app = express()
 
+//чтобы сервер понимал json
 app.use(express.json())
 
 app.get('/', roleMiddleware(["USER"]), async (req, res) => {
@@ -15,10 +19,16 @@ app.get('/', roleMiddleware(["USER"]), async (req, res) => {
     res.send(data)
 })
 
+//ветка регистрации
 app.post('/reg', register)
+//ветка логина
 app.post('/auth', auth)
 
+
+//функция старта приложения
 const start = async () => {
+
+    //создаем таблицы
     await sql`create table if not exists Roles(
         role varchar(100) unique primary key
     )`
@@ -29,8 +39,16 @@ const start = async () => {
         password varchar(100),
         FOREIGN KEY (role) REFERENCES Roles(role)
     )`
+
+    //запустить в первый раз и больше не запускать
+    //чтобы добавить роли в таблицу ролей
+
     // await sql`insert into Roles(role) values('USER')`
     // await sql`insert into Roles(role) values('ADMIN')`
+
+    //запустить сервак
+    //(прослушивать порт на запросы)
+    //вторым аргументом функция которая запустится при успешном запуске сервака
     app.listen(PORT, () => {
         console.log(`СЕРВАК ФУРЫЧИТ ТУТ http://localhost:${PORT}`);
     })
